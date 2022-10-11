ba448c4903dc005da7997cb3cde1451577c963d0
JhonatanLop <jhooliveira.lopes@gmail.com>
2022-10-10 21:02:57 -0300

    pega marcão

---
+++ graficoteste.py
@@ -0,0 +1,23 @@
+# importando matplitlib
+from ast import NodeTransformer
+from xml.dom.minidom import Notation
+import matplotlib.pyplot as plt
+
+# declarando as listas com os valores que serão usados no gráfico
+sprint = [1, 2, 3, 4]
+notas1 = [2, 2, 2, 2, 2]
+notas2 = [1, 1, 1, 1, 1]
+
+# titulo do gráfico
+plt.title("Primeiro gráfico")
+
+# adicionando labels no eixo x e y
+plt.xlabel("valores postos")
+plt.ylabel("meses postos")
+
+# inicializando a tela com o gráfico
+plt.plot(meses, valores, valores2)
+plt.show()
+
+nota 
+id  nota
