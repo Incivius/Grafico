b86dbc054febd092e6667500ce3d5bba6f797561
Incivius <111438883+Incivius@users.noreply.github.com>
2022-10-10 21:28:27 -0300

    Pega ai jhownjhown

--- graficoteste.py
+++ graficoteste.py
@@ -1,12 +1,22 @@
 # importando matplitlib
-from ast import NodeTransformer
-from xml.dom.minidom import Notation
+from statistics import median
 import matplotlib.pyplot as plt
 
 # declarando as listas com os valores que serão usados no gráfico
-sprint = [1, 2, 3, 4]
-notas1 = [2, 2, 2, 2, 2]
-notas2 = [1, 1, 1, 1, 1]
+sprint = ['Sprint1', 'Sprint2', 'Sprint3']
+
+notas1 = [2, 4, 5]
+notas2 = [1, 2, 2]
+notas3 = [2, 3, 4]
+notas4 = [4, 5, 5]
+notas5 = [5, 5, 5]
+
+Mnotas1 = int(median(notas1))
+Mnotas2 = int(median(notas2))
+Mnotas3 = int(median(notas3))
+Mnotas4 = int(median(notas4))
+Mnotas5 = int(median(notas5))
+
 
 # titulo do gráfico
 plt.title("Primeiro gráfico")
@@ -16,8 +26,5 @@ plt.xlabel("valores postos")
 plt.ylabel("meses postos")
 
 # inicializando a tela com o gráfico
-plt.plot(meses, valores, valores2)
+plt.plot(sprint,Mnotas1)
 plt.show()
-
-nota 
-id  nota
