40af53721238057b1379c275fa903cb5661c1bab
JhonatanLop <jhooliveira.lopes@gmail.com>
2022-10-10 22:17:16 -0300

    pega marcão lindooo

--- graficoteste.py
+++ graficoteste.py
@@ -1,23 +1,27 @@
 # importando matplitlib
 from ast import NodeTransformer
-from xml.dom.minidom import Notation
-import matplotlib.pyplot as plt
+from turtle import color
+import matplotlib.pyplot as plt 
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
 
 # declarando as listas com os valores que serão usados no gráfico
-sprint = [1, 2, 3, 4]
-notas1 = [2, 2, 2, 2, 2]
-notas2 = [1, 1, 1, 1, 1]
+criterio = ["trbalho em equipe", "produtividade", "depressão", "anciedade", "vontade de viver"]
+sprint = [2, 3, 4, 1, 5]
+sprint2 = [3, 1, 4, 5, 1]
 
 # titulo do gráfico
 plt.title("Primeiro gráfico")
+
+ax.set_ylim([0, 5.2])
 
 # adicionando labels no eixo x e y
 plt.xlabel("valores postos")
 plt.ylabel("meses postos")
 
 # inicializando a tela com o gráfico
-plt.plot(meses, valores, valores2)
+plt.bar(criterio, sprint, color = "red")
+plt.bar(criterio, sprint2, color = "purple")
 plt.show()
-
-nota 
-id  nota
